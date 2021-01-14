ee9b1ebe6741466ecfe5a1fb7655f558a1bbf8e9
ishuacharis <ishuacharis@gmail.com>
2021-01-14 17:20:08 +0100

    modified the signupSchema

--- src/lib/ValidationSchema/schema.js
+++ src/lib/ValidationSchema/schema.js
@@ -1,7 +1,9 @@
 import * as Yup from 'yup'
 
 const signUpSchema  = Yup.object().shape({
+    name: Yup.string().required("Required"),
     email: Yup.string().email('Inavlid Email').required("Required"),
+    phoneNo: Yup.string().required("Required"),
     password: Yup.string().required('Required').min(4, 'Too short').max(10, 'Too long'),
     confirmPassword: Yup.string().test('is-password','Password does not match', 
     (value, context) => value === context.parent.password)
