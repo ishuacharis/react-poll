6b0d1f80385bccb9cd7df04057c1700f8f1cc394
ishuacharis <ishuacharis@gmail.com>
2021-02-23 11:06:19 +0100

    change TOKEN and USER to REACT_TOKEN and REACT_USER

--- src/lib/redux/actions/action_creators/auth/auth_async_actions.js
+++ src/lib/redux/actions/action_creators/auth/auth_async_actions.js
@@ -5,13 +5,12 @@ import { deleteToken, deleteUser, setLoading, setToken, setUser } from "./auth";
 export const authenticate  = (details) => {
     
     return async function login (dispatch, getState){
-        
         try {
             const response  = await handleLogin(details)
             const user  = response['response']['user'];
             const token  = response['response']['token'];
-            localStorage.setItem('USER', JSON.stringify(user));
-            localStorage.setItem('TOKEN', JSON.stringify(token));
+            localStorage.setItem('REACT_USER', JSON.stringify(user));
+            localStorage.setItem('REACT_TOKEN', JSON.stringify(token));
             dispatch(setUser(user))
             dispatch(setToken(token))
         } catch (error) {
@@ -29,8 +28,8 @@ export const unAuthenticate =  (details) => {
         const response  = await handleLogOut(details)
         if ('response' in response) {
 
-            localStorage.removeItem('USER')
-            localStorage.removeItem('TOKEN')
+            localStorage.removeItem('REACT_USER')
+            localStorage.removeItem('REACT_TOKEN')
             dispatch(deleteUser())
             dispatch(deleteToken())
             dispatch(setLoading(false))
--- src/lib/redux/states/auth.js
+++ src/lib/redux/states/auth.js
@@ -1,5 +1,5 @@
 export const authInitialState = {
-    user: JSON.parse(localStorage.getItem('USER')) || null,
-    token: JSON.parse(localStorage.getItem('TOKEN')) || null,
+    user: JSON.parse(localStorage.getItem('REACT_USER')) || null,
+    token: JSON.parse(localStorage.getItem('REACT_TOKEN')) || null,
     isLoading: false
 }
