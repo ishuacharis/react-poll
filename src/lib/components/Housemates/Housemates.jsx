24ddb1641dbb932487be29273e95c7682bee6651
ishuacharis <ishuacharis@gmail.com>
2021-03-20 22:13:02 +0100

    replace useState with useReducer

--- src/lib/components/Housemates/Housemates.jsx
+++ src/lib/components/Housemates/Housemates.jsx
@@ -1,17 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useReducer } from 'react'
 import PropTypes from 'prop-types'
 import VoteContext from '../../Context/VoteContext'
 import './Housemates.scoped.css'
 import Housemate from '../Housemate/Housemate.jsx'
 import { eviction } from 'lib/routes'
 
-
+function houseMatesReducer(state, {type, payload: { houseMates, isLoading} }) {
+    switch (type) {
+        case "set_evictionList":
+            return  { ...state, houseMates: houseMates }
+        case "setIsLoading":
+            return { ...state, isLoading: isLoading }
+        default:
+            throw new Error();
+    }
+}
 function Housemates() {
 
     const {houseMates,} = useContext(VoteContext);
-    const [ evictionList , setEvictionList ] = useState([])
-    const [ isLoading, setLoading ] = useState(true);
-
+    const [ state , dispatch] = useReducer(houseMatesReducer,{houseMates: [], isLoading: true });
     const args = {
         endPoint: "/eviction",
         method: 'GET',
@@ -21,20 +28,20 @@ function Housemates() {
     const getEvictionList =  async () => {
         const {response: {data}}  = await eviction(args)
         localStorage.setItem('REACT_HOUSEMATES', JSON.stringify(data))
-        setEvictionList(data)
-        setLoading(false)
+        dispatch({ type: "set_evictionList", payload: { houseMates: data } })
+        dispatch({ type: "setIsLoading", payload: { isLoading: false } })
+        
     }
     useEffect(() => {
         getEvictionList()
     }, [])
 
-
-    const houses = evictionList.map((houseMateUpForEviction) =>
+    const houses = state.houseMates.map((houseMateUpForEviction) =>
     <Housemate
         houseMateUpForEviction= {houseMateUpForEviction}
         key={houseMateUpForEviction.id}  />
     )
-    if(isLoading) return (
+    if(state.isLoading) return (
         <div className="loading-container">
           <div className="loading"></div>
         </div>
