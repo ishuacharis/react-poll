44eaa6ae25c7cc36abd3a74968c126be5fe881f8
ishuacharis <ishuacharis@gmail.com>
2021-03-29 16:17:47 +0100

    modify css and components

--- src/lib/components/shared/SideNav/SideNav.jsx
+++ src/lib/components/shared/SideNav/SideNav.jsx
@@ -3,54 +3,128 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './SideNav.scoped.css';
-function SideNav({ user }) {
+function SideNav({ user:{name} }) {
     return (
         <div className="sidenav">
-            <ul className="sidenav-links">
-                <div className="sidenav-top">
-                    <li className="sidenav-link">
-                        <Link to="/project/house" className="sidenav-item">
-                            Home
-                        </Link>
-                    </li>
-                    <li className="sidenav-link">
-                        <Link to="/project/testing" className="sidenav-item">
-                            Dashboard
-                        </Link>
-                    </li>
-                    <li className="sidenav-link">
-                        <Link to="#" className="sidenav-item">
-                            Profile
-                        </Link>
-                    </li>
-                    <li className="sidenav-link">
-                        <Link to="/notifications" className="sidenav-item">
-                            Notifications
-                        </Link>
-                    </li>
-                    <li className="sidenav-link">
-                        <Link to="#" className="sidenav-item">
-                            Messages
-                        </Link>
-                    </li>
-                </div>
-                <div className="bottom">
-                    <li className="sidenav-link">
-                        <Link to="/logout" className="sidenav-item">
-                            <div className="logout">
+            <div className="logout__tooltip">
+                <div className="logout-tooltip__content">
+                    <div className="logout-tooltip__item">
+                        <div className="logout">
+                            <div className="logout-left">
                                 <div className="avatar avatar-small">
-                                    <img src={require("lib/assets/vee.jpg")} alt="avatar"/>
+                                    <img src={require("lib/assets/boy.jpg")} />
                                 </div>
                                 <div className="handle">
-                                    <span> { user.name } </span>
-                                    <span>@Olawaley</span>
+                                    <span>{ name }</span>
+                                    <span>@{name}</span>
                                 </div>
                             </div>
-                        </Link>
-                    </li>
+                            <div className="logout-right">
+                                <i className='bx bx-check'></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="logout-tooltip__item">
+                        <div className="logout">
+                            <div className="logout-left">
+                                <Link to="/" className="bottom-sidenav__item">
+                                    <li className="bottom-sidenav__link">
+                                        Add an existing account
+                                    </li>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="logout-tooltip__item">
+                        <div className="logout">
+                            <div className="logout-left">
+                                <div className="bottom-sidenav__link">
+                                    <Link to="/" className="bottom-sidenav__item">
+                                        Log out @wale
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </ul>
+            </div>
+            
+            <div className="sidenav-top">
+                <ul className="sidenav-links">
+                    <Link to="/housemates" className="sidenav-item">
+                        <li className="sidenav-link">
+                            <span>
+                                <i className='bx bx-home'></i>
+                            </span>
+                            <span>
+                                Home
+                            </span>
+                        </li>                        
+                    </Link>
+                    <Link to="/" className="sidenav-item">
+                        <li className="sidenav-link">
+                            <span>
+                                <i className='bx bx-folder'></i>
+                            </span>
+                            <span>
+                                Dashboard
+                            </span>
+                        </li> 
+                    </Link>
+                    <Link to="/profile" className="sidenav-item">
+                        <li className="sidenav-link">
+                            <span>
+                                <i className='bx bx-user'></i>  
+                            </span>
+                            <span>
+                                Profile
+                            </span>
+                        </li>
+                    </Link>
+                    <Link to="/notifications" className="sidenav-item">
+                        <li className="sidenav-link">
+                            <span>
+                                <span><i className='bx bx-bell'></i></span>
+                            </span>
+                            <span>
+                                Notifications
+                            </span>
+                        </li>
+                    </Link>
+                    <Link to="/" className="sidenav-item">
+                        <li className="sidenav-link">
+                            <span>
+                                <i className='bx bx-envelope'></i>
+                            </span>
+                            <span>
+                                Messages
+                            </span>
+                        </li>
+                    </Link>               
+                </ul>
         </div>
+            <div className="bottom">
+                <li className="sidenav__link-bottom">
+                    <button type="button" className="bottom-logout__btn">
+                        <div className="logout">
+                            <div className="logout-left">
+                                <div className="avatar avatar-small">
+                                    <img src={require("lib/assets/boy.jpg")} />
+                                </div>
+                                <div className="handle">
+                                    <span>{ name }</span>
+                                    <span>@ { name } </span>
+                                </div>
+                            </div>
+                            <div className="logout-right">
+                                <i className='bx bx-dots-horizontal-rounded'></i>
+                            </div>
+                        </div>
+                    </button>
+                </li>
+            </div>
+
+    </div>
     )
 }
 
