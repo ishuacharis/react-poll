bf41160928f781f7a305ee34aa9bdfdda13f0b63
ishuacharis <ishuacharis@gmail.com>
2021-04-22 01:23:57 +0100

    modify schema export

--- src/lib/ValidationSchema/schema.js
+++ src/lib/ValidationSchema/schema.js
@@ -1,19 +1,18 @@
 import * as Yup from 'yup'
 
-const signUpSchema  = Yup.object().shape({
-    name: Yup.string().required("Required"),
-    email: Yup.string().email('Inavlid Email').required("Required"),
-    phoneNo: Yup.string().required("Required"),
-    password: Yup.string().required('Required').min(4, 'Too short').max(10, 'Too long'),
+export const signUpSchema  = Yup.object().shape({
+    name: Yup.string().required().label("Name"),
+    email: Yup.string().email('Inavlid email').required().label("Email"),
+    phoneNo: Yup.string().required().label('Phone'),
+    password: Yup.string().required().min(4, 'Too short').max(10, 'Too long').label("Password"),
     confirmPassword: Yup.string().test('is-password','Password does not match', 
     (value, context) => value === context.parent.password)
 })
 
-const loginSchema  = Yup.object().shape({
-    email: Yup.string().email('Inavlid Email').required("Required"),
-    password: Yup.string().required('Required').min(4, 'Too short').max(10, 'Too long'),
+export const loginSchema  = Yup.object().shape({
+    email: Yup.string().email("Inavlid email").required().label("Email"),
+    password: Yup.string().required().min(4, 'Too short').max(10, 'Too long').label('Password'),
 })
 
-export {
-    signUpSchema,loginSchema
-}
+export const loginFormValues = {email: '', password: ''};
+export const registerFormValues  = {name: '', email: '', phoneNo: '', password: '', confirmPassword: ''};
