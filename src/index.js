fba42b8dbcbaf55453c4ef58a25144b6c6310007
ishuacharis <ishuacharis@gmail.com>
2021-04-29 17:41:36 +0100

    wrapp root main with persitgate

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import App from './App';
+//import App from './App';
+import Main from './Main';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { configureStore } from 'lib/redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 
-const store  = configureStore(undefined);
+const {store,persistor}  = configureStore(undefined);
 
 console.log(store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <Main />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
